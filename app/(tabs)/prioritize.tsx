56dd26c6d3dcd68c0971f1c7ae274cc62abe7679
Drew <sirampr@gmail.com>
2025-03-07 13:15:30 -0500

    add svg lines

--- app/(tabs)/prioritize.tsx
+++ app/(tabs)/prioritize.tsx
@@ -8,6 +8,7 @@ import Animated, {
   interpolateColor,
 } from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Svg, { Line } from "react-native-svg";
 
 import { ShadowView } from "@/components/shadow-view";
 import { colors } from "@/constants/Colors";
@@ -58,10 +59,7 @@ export default function PrioritizeScreen() {
     .onUpdate((event) => {
       translateX.value = event.translationX;
       translateY.value = event.translationY;
-
       const distance = getDistance(event.translationX, event.translationY);
-      console.log(distance);
-
       colorProgress.value = Math.min(distance / (RADIUS * 0.7), 1);
     })
     .onEnd(({ translationX, translationY }) => {
@@ -79,37 +77,63 @@ export default function PrioritizeScreen() {
     .runOnJS(true);
 
   return (
-    <SafeAreaView className="flex-1 bg-background p-6">
+    <SafeAreaView className="flex-1 bg-background">
       <StatusBar style="dark" />
-      <Text className="font-public-sans-bold text-4xl text-primary">
-        Prioritize
-      </Text>
-      <Text className="pt-1 font-public-sans-light text-secondary">
-        Drag tasks to quadrants to prioritize.{" "}
-        <Text className="underline">Learn more</Text> about Eisenhower Matrix.
-      </Text>
-      <View className="absolute left-0 top-0 h-full w-full border-t border-dashed border-secondary" />
-      <GestureDetector gesture={pan}>
-        <Animated.View
-          className="flex-1 items-center justify-center"
-          style={animatedViewStyles}
-        >
-          <ShadowView
-            className="relative h-[250px] w-[250px]"
-            style={{ shadowOffset: { height: 4, width: 4 } }}
-          >
-            <Animated.View
-              className="absolute size-full"
-              style={[animatedColorStyle]}
+      <View className="p-6">
+        <Text className="font-public-sans-bold text-4xl text-primary">
+          Prioritize
+        </Text>
+        <Text className="pt-1 font-public-sans-light text-secondary">
+          Drag tasks to quadrants to prioritize.{" "}
+          <Text className="underline">Learn more</Text> about Eisenhower Matrix.
+        </Text>
+      </View>
+      <View className="flex-1">
+        <View className="absolute inset-0 flex items-center justify-center">
+          <Svg height="1" width="100%">
+            <Line
+              x1="0"
+              y1="0"
+              x2="100%"
+              y2="0"
+              stroke={colors.primary}
+              strokeDasharray="10 5"
             />
-            <View className="px-7 py-9">
-              <Text className="font-public-sans-bold text-4xl text-primary">
-                Write blog post
-              </Text>
-            </View>
-          </ShadowView>
-        </Animated.View>
-      </GestureDetector>
+          </Svg>
+          <View className="absolute h-full">
+            <Svg height="100%" width="1">
+              <Line
+                x1="0"
+                y1="0"
+                x2="0"
+                y2="100%"
+                stroke={colors.primary}
+                strokeDasharray="10 5"
+              />
+            </Svg>
+          </View>
+        </View>
+        <View className="flex-1 items-center justify-center">
+          <GestureDetector gesture={pan}>
+            <Animated.View style={animatedViewStyles}>
+              <ShadowView
+                className="relative h-[250px] w-[250px]"
+                style={{ shadowOffset: { height: 4, width: 4 } }}
+              >
+                <Animated.View
+                  className="absolute size-full"
+                  style={[animatedColorStyle]}
+                />
+                <View className="px-7 py-9">
+                  <Text className="font-public-sans-bold text-4xl text-primary">
+                    Write blog post
+                  </Text>
+                </View>
+              </ShadowView>
+            </Animated.View>
+          </GestureDetector>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
