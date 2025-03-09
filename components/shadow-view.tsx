8ff5836301a8ef9382967b3c307cf97624b33407
Drew <sirampr@gmail.com>
2025-03-08 20:00:08 -0500

    add forward ref

--- components/shadow-view.tsx
+++ components/shadow-view.tsx
@@ -1,35 +1,36 @@
 import clsx from "clsx";
-import { View, ViewStyle } from "react-native";
+import { forwardRef } from "react";
+import { View, ViewProps } from "react-native";
 
 import { colors } from "@/constants/Colors";
 
-export function ShadowView({
-  className,
-  style,
-  children,
-}: {
-  className?: string;
-  style?: ViewStyle;
-  children?: React.ReactNode;
-}) {
-  return (
-    <View
-      style={{
-        shadowOffset: {
-          height: 1,
-          width: 1,
-        },
-        shadowColor: colors.primary,
-        shadowOpacity: 1,
-        shadowRadius: 0,
-        ...style,
-      }}
-      className={clsx([
-        "rounded-full border border-primary bg-background",
-        className,
-      ])}
-    >
-      {children}
-    </View>
-  );
-}
+export const ShadowView = forwardRef<View, ViewProps>(
+  ({ className, style, children, ...props }, ref) => {
+    return (
+      <View
+        ref={ref}
+        style={[
+          {
+            shadowOffset: {
+              height: 1,
+              width: 1,
+            },
+            shadowColor: colors.primary,
+            shadowOpacity: 1,
+            shadowRadius: 0,
+          },
+          style,
+        ]}
+        {...props}
+        className={clsx([
+          "rounded-full border border-primary bg-background",
+          className,
+        ])}
+      >
+        {children}
+      </View>
+    );
+  },
+);
+
+ShadowView.displayName = "ShadowView";
