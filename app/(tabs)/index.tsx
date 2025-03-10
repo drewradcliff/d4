8d234aa97a0236dac79066e2fd4b8780fa55d8c5
Drew Radcliff <sirampr@gmail.com>
2025-03-10 16:11:22 -0400

    feat: edit inbox (#9)
    * wip
    
    * add edit tasks
    
    * fix formatting errors
    
    * refactor conditions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { Feather } from "@expo/vector-icons";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { isNull } from "drizzle-orm";
+import clsx from "clsx";
+import { eq, isNull } from "drizzle-orm";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import { FlatList, Pressable, Text, TextInput, View } from "react-native";
@@ -11,7 +12,7 @@ import { queryClient } from "../_layout";
 import { ShadowView } from "@/components/shadow-view";
 import { colors } from "@/constants/colors";
 import { db } from "@/db/client";
-import { tasks } from "@/db/schema";
+import { Task, tasks } from "@/db/schema";
 
 export default function InboxScreen() {
   const [description, setDescription] = useState("");
@@ -21,6 +22,7 @@ export default function InboxScreen() {
     queryFn: async () =>
       await db.select().from(tasks).where(isNull(tasks.priority)),
   });
+
   const { mutate: addTask } = useMutation({
     mutationFn: async () => {
       if (!description.trim()) return;
@@ -38,11 +40,13 @@ export default function InboxScreen() {
       <Text className="font-public-sans-bold text-4xl text-primary">Inbox</Text>
       <View className="flex-row items-center gap-2 pt-5">
         <TextInput
-          className="text flex-1 border border-primary bg-white p-3 font-public-sans-light"
+          className="flex-1 border border-primary bg-white p-3 font-public-sans-light text-primary"
           placeholder="Add task..."
           placeholderTextColor={colors.secondary}
           value={description}
           onChangeText={setDescription}
+          onSubmitEditing={() => addTask()}
+          submitBehavior="submit"
         />
         <Pressable onPress={() => addTask()}>
           {({ pressed }) => (
@@ -68,15 +72,114 @@ export default function InboxScreen() {
         data={data}
         keyExtractor={(item) => item.id.toString()}
         contentContainerStyle={{ gap: 16, paddingTop: 20 }}
-        renderItem={({ item }) => (
-          <View className="flex-row items-center gap-3">
-            <ShadowView className="h-4 w-4 rounded-full" />
-            <Text className="font-public-sans-light text-lg">
-              {item.description}
-            </Text>
-          </View>
-        )}
+        renderItem={({ item }) => <TaskItem item={item} />}
       />
     </SafeAreaView>
+  );
+}
+
+function TaskItem({ item }: { item: Task }) {
+  const [description, setDescription] = useState(item.description);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const { mutate: updateTask } = useMutation({
+    mutationFn: async ({
+      id,
+      description,
+    }: {
+      id: number;
+      description: string;
+    }) => {
+      if (!description.trim()) return;
+      await db.update(tasks).set({ description }).where(eq(tasks.id, id));
+    },
+    onSuccess: () => {
+      setIsEditing(false);
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
+
+  const { mutate: deleteTask } = useMutation({
+    mutationFn: async () => {
+      await db.delete(tasks).where(eq(tasks.id, item.id));
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
+
+  const { mutate: toggleTask } = useMutation({
+    mutationFn: async ({
+      id,
+      completedAt,
+    }: {
+      id: number;
+      completedAt: string | null;
+    }) => {
+      await db
+        .update(tasks)
+        .set({ completedAt: completedAt ?? null })
+        .where(eq(tasks.id, id));
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
+
+  return (
+    <Pressable onPress={() => setIsEditing(!isEditing)}>
+      <View className="flex-row items-center gap-3">
+        <Pressable
+          onPress={() => {
+            toggleTask({
+              id: item.id,
+              completedAt: item.completedAt ? null : new Date().toISOString(),
+            });
+          }}
+        >
+          <ShadowView className="h-4 w-4 items-center justify-center rounded-full">
+            {item.completedAt && (
+              <Feather name="check" size={12} color={colors.primary} />
+            )}
+          </ShadowView>
+        </Pressable>
+        {isEditing ? (
+          <View className="flex-1 flex-row gap-2 pr-3">
+            <TextInput
+              className="flex-1 font-public-sans-light text-primary"
+              autoFocus
+              value={description}
+              onChangeText={setDescription}
+              onBlur={() => {
+                if (!description.trim()) deleteTask();
+                updateTask({ id: item.id, description });
+              }}
+              onKeyPress={({ nativeEvent }) => {
+                if (nativeEvent.key === "Enter") {
+                  if (!description.trim()) deleteTask();
+                  updateTask({ id: item.id, description });
+                } else if (
+                  nativeEvent.key === "Backspace" &&
+                  !description.trim()
+                )
+                  deleteTask();
+              }}
+            />
+            <Pressable onPress={() => deleteTask()}>
+              <Feather name="x" size={18} color={colors.primary} />
+            </Pressable>
+          </View>
+        ) : (
+          <Text
+            className={clsx(
+              "font-public-sans-light text-primary",
+              item.completedAt && "line-through",
+            )}
+          >
+            {description}
+          </Text>
+        )}
+      </View>
+    </Pressable>
   );
 }
