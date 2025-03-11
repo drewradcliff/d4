7a3139390f1685c28031508b58dd0d649fa2de94
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2025-03-11 13:43:56 -0400

    refactor: update tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,39 +6,35 @@ import {
   TabTrigger,
   TabTriggerSlotProps,
 } from "expo-router/ui";
-import { ComponentProps, forwardRef, Ref } from "react";
-import { Pressable, Text, TextProps, View } from "react-native";
+import { ComponentProps, forwardRef } from "react";
+import { Pressable, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { ShadowView } from "@/components/shadow-view";
 import { TabBackgroundSvg } from "@/components/tab-background-svg";
 import { theme } from "@/styles/theme";
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <Tabs className="bg-background pb-10">
+    <Tabs>
       <TabSlot />
       <TabList asChild>
         <ShadowView
-          style={{ shadowOffset: { width: 2, height: 2 } }}
-          className="mx-3 rounded-full px-10 py-6"
+          className="mx-3 rounded-full bg-background px-10 py-6"
+          style={{
+            marginBottom: insets.bottom,
+            shadowOffset: { width: 2, height: 2 },
+          }}
         >
           <TabTrigger name="index" href="/" asChild>
             <TabButton icon="inbox">Inbox</TabButton>
           </TabTrigger>
-          <TabTrigger
-            className="flex-row items-center gap-2"
-            name="prioritize"
-            href="/prioritize"
-            asChild
-          >
+          <TabTrigger name="prioritize" href="/prioritize" asChild>
             <TabButton icon="zap">Prioritize</TabButton>
           </TabTrigger>
-          <TabTrigger
-            className="flex-row items-center gap-2"
-            name="tasks"
-            href="/tasks"
-            asChild
-          >
+          <TabTrigger name="tasks" href="/tasks" asChild>
             <TabButton icon="list">Tasks</TabButton>
           </TabTrigger>
         </ShadowView>
@@ -47,31 +43,17 @@ export default function TabLayout() {
   );
 }
 
-type Icon = ComponentProps<typeof Feather>["name"];
-
-export type TabButtonProps = TabTriggerSlotProps & {
-  icon?: Icon;
-  children: TextProps["children"];
-};
-
-const TabButton = forwardRef(
-  ({ icon, children, isFocused, ...props }: TabButtonProps, ref: Ref<View>) => {
-    return (
-      <Pressable
-        ref={ref}
-        {...props}
-        className="relative flex-row items-center gap-2"
-      >
-        {isFocused && (
-          <View className="absolute -left-8">
-            <TabBackgroundSvg />
-          </View>
-        )}
-        {icon && <Feather color={theme.colors.primary} name={icon} size={18} />}
-        <Text className="font-public-sans-regular text-secondary">
-          {children}
-        </Text>
-      </Pressable>
-    );
-  },
-);
+const TabButton = forwardRef<
+  View,
+  TabTriggerSlotProps & { icon: ComponentProps<typeof Feather>["name"] }
+>(({ icon, children, isFocused, ...props }, ref) => (
+  <Pressable className="flex-row items-center gap-2" ref={ref} {...props}>
+    {isFocused && (
+      <View className="absolute -left-8">
+        <TabBackgroundSvg />
+      </View>
+    )}
+    <Feather color={theme.colors.primary} name={icon} size={18} />
+    <Text className="font-public-sans-regular text-secondary">{children}</Text>
+  </Pressable>
+));
