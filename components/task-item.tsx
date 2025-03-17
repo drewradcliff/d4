c1efc5f3f35a5161542e42c75a71786df79b2059
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2025-03-17 10:41:44 -0400

    refactor: componentize task item (#14)

---
+++ components/task-item.tsx
@@ -0,0 +1,78 @@
+import { Feather } from "@expo/vector-icons";
+import clsx from "clsx";
+import { eq } from "drizzle-orm";
+import { useState } from "react";
+import { Pressable, TextInput, View } from "react-native";
+
+import { Paper } from "@/components/paper";
+import { db } from "@/db/client";
+import { Task, tasks } from "@/db/schema";
+import { theme } from "@/styles/theme";
+
+export function TaskItem({ task }: { task: Task }) {
+  const [description, setDescription] = useState(task.description);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const isEmpty = !description.trim();
+
+  const updateTask = async () => {
+    if (isEmpty) {
+      await deleteTask();
+    } else {
+      await db.update(tasks).set({ description }).where(eq(tasks.id, task.id));
+    }
+  };
+
+  const deleteTask = async () => {
+    await db.delete(tasks).where(eq(tasks.id, task.id));
+  };
+
+  const toggleTask = async () => {
+    await db
+      .update(tasks)
+      .set({ completedAt: task.completedAt ? null : new Date().toISOString() })
+      .where(eq(tasks.id, task.id));
+  };
+
+  return (
+    <View className="flex-row items-center gap-3">
+      <Pressable hitSlop={8} onPress={toggleTask}>
+        <Paper className="size-10 items-center justify-center rounded-full">
+          {task.completedAt && (
+            <Feather name="check" size={16} color={theme.colors.primary} />
+          )}
+        </Paper>
+      </Pressable>
+      <View className="flex-1 flex-row gap-2">
+        <TextInput
+          className={clsx(
+            "flex-1 font-public-sans-light text-xl leading-[0] text-primary",
+            task.completedAt && "line-through",
+          )}
+          editable={!task.completedAt}
+          value={description}
+          onChangeText={setDescription}
+          onEndEditing={updateTask}
+          onSubmitEditing={updateTask}
+          onBlur={() => setIsFocused(false)}
+          onFocus={() => setIsFocused(true)}
+          onKeyPress={({ nativeEvent }) => {
+            if (nativeEvent.key === "Backspace" && isEmpty) {
+              deleteTask();
+            }
+          }}
+        />
+        <Pressable
+          hitSlop={8}
+          className={clsx(
+            "size-12 items-center justify-center",
+            isFocused ? "visible" : "invisible",
+          )}
+          onPress={deleteTask}
+        >
+          <Feather name="x" size={18} color={theme.colors.primary} />
+        </Pressable>
+      </View>
+    </View>
+  );
+}
